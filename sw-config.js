da30123eb518f280a46301c4e3432eee388d64ae
Alex J <alexjovermorales@gmail.com>
2017-07-27 19:02:14 +0200

    Add PWA support

---
+++ sw-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  staticFileGlobs: [
+    'public/css/**.css',
+    'public/**.html',
+    'public/**.jpg',
+    'public/**.png',
+    'public/js/**.js'
+  ],
+  stripPrefix: 'public',
+}
